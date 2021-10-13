fc4fcd41fa3876debe5b5947ceefe3cf69574be0
Joanna <m.joanna.foss@gmail.com>
2021-10-13 15:44:19 -0500

    begin react practice

---
+++ react-tutorial/src/Table.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react'
+
+// class Table extends Component {
+//   render() {
+//     return (
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>Job</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           <tr>
+//             <td>Charlie</td>
+//             <td>Janitor</td>
+//           </tr>
+//           <tr>
+//             <td>Mac</td>
+//             <td>Bouncer</td>
+//           </tr>
+//           <tr>
+//             <td>Dee</td>
+//             <td>Aspiring actress</td>
+//           </tr>
+//           <tr>
+//             <td>Dennis</td>
+//             <td>Bartender</td>
+//           </tr>
+//         </tbody>
+//       </table>
+//     )
+//   }
+// }
+
+const Tableheader = () => {
+  return (
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+}
+
+const Tablebody = () => {
+  return (
+    <tbody>
+      <tr>
+        <td>Charlie</td>
+        <td>Janitor</td>
+      </tr>
+      <tr>
+        <td>Mac</td>
+        <td>Bouncer</td>
+      </tr>
+      <tr>
+        <td>Dee</td>
+        <td>Actress</td>
+      </tr>
+      <tr>
+        <td>Denise</td>
+        <td>Bartender</td>
+      </tr>
+    </tbody>
+  )
+}
+
+class Table extends Component {
+  render() {
+    return (
+      <table>
+        <Tableheader />
+        <Tablebody />
+      </table>
+    )
+  }
+}
+
+export default Table
