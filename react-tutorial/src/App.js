814651e3377a15bc233f85e9de225808a592a333
Joanna <m.joanna.foss@gmail.com>
2021-11-16 10:00:49 -0600

    feat: in-prog React proj

--- react-tutorial/src/App.js
+++ react-tutorial/src/App.js
@@ -1,17 +1,37 @@
 import React, {Component} from "react"
 import Table from "./Table"
 
-// class App extends Component {
-// 	render() {
-// 		return (
-// 			<div class="container">
-// 				<Table />
-// 			</div>
-// 		);
-// 	}
-// }
-
 class App extends Component {
+	state = {
+		characters: [
+			{
+				name: 'Charlie',
+				job: 'Janitor',
+			},
+			{
+				name: 'Mac',
+				job: 'Bouncer',
+			},
+			{
+				name: 'Dee',
+				job: 'Actress',
+			},
+			{
+				name: 'Dennis',
+				job: 'Bartender',
+			},
+		],
+	}
+
+	removeCharacter = (index) => {
+		const {characters} = this.state
+		this.setState({
+			characters: characters.filters((character, i) => {
+				return i !== index
+			}),
+		})
+	}
+
 	render() {
 		const characters = [
 			{
--- react-tutorial/src/Table.js
+++ react-tutorial/src/Table.js
@@ -1,38 +1,5 @@
 import React, {Component} from 'react'
 
-// class Table extends Component {
-//   render() {
-//     return (
-//       <table>
-//         <thead>
-//           <tr>
-//             <th>Name</th>
-//             <th>Job</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           <tr>
-//             <td>Charlie</td>
-//             <td>Janitor</td>
-//           </tr>
-//           <tr>
-//             <td>Mac</td>
-//             <td>Bouncer</td>
-//           </tr>
-//           <tr>
-//             <td>Dee</td>
-//             <td>Aspiring actress</td>
-//           </tr>
-//           <tr>
-//             <td>Dennis</td>
-//             <td>Bartender</td>
-//           </tr>
-//         </tbody>
-//       </table>
-//     )
-//   }
-// }
-
 const Tableheader = () => {
   return (
     <thead>
@@ -44,10 +11,17 @@ const Tableheader = () => {
   )
 }
 
-const Tablebody = () => {
-  return (
-    <tbody />
-  )
+const Tablebody = (props) => {
+    const rows = props.characterData.map((row, index) => {
+        return (
+            <tr key={index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    })
+
+    return <tbody>{rows}</tbody>
 }
 
 class Table extends Component {
