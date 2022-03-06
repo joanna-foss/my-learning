e3ff91cd484c13914dcff8892528815c0a20dd86
Joanna <m.joanna.foss@gmail.com>
2022-03-05 23:27:58 -0600

    sliding window edits; optimal solution

--- miscellaneous/sliding-window.js
+++ miscellaneous/sliding-window.js
@@ -1,4 +1,4 @@
-//BRUTE FORCE METHOD
+//BRUTE FORCE METHOD O(n*k)
 const max_sub_array_of_size_k = function(k, arr) {
     maxSum = -1;
     // windowSums = [];
@@ -8,9 +8,7 @@ const max_sub_array_of_size_k = function(k, arr) {
           windowSum+=arr[j];
       }
       // windowSums.push(windowSum);
-      if(windowSum > maxSum) {
-          maxSum = windowSum;
-      }
+      maxSum = Math.max(windowSum, maxSum);
     }
     // console.log(windowSums);
     return maxSum;
@@ -18,3 +16,25 @@ const max_sub_array_of_size_k = function(k, arr) {
 
   console.log(max_sub_array_of_size_k(3, [1,5,2,3,4])); //10
   console.log(max_sub_array_of_size_k(5, [9,-3,2,22,-9,8,1,2])); //24
+
+  //SLIDING WINDOW METHOD O(n)
+  const max_sub_array_of_size_l = (l, arr) => {
+    maxSum = 0;
+    windowSum = 0;
+    windowStart = 0;
+
+    for(windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+      windowSum += arr[windowEnd];
+
+      if(windowEnd >= l-1) {
+        maxSum = Math.max(maxSum, windowSum);
+        windowSum -= arr[windowStart];
+        windowStart++;
+      }
+    }
+
+    return maxSum;
+  }
+
+  console.log(max_sub_array_of_size_l(3, [1,5,2,3,4])); //10
+  console.log(max_sub_array_of_size_l(5, [9,-3,2,22,-9,8,1,2])); //24
