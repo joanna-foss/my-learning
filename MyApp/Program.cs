d7ae066ff485c7eae4f2fba22659a0e23d0c352f
Joanna <m.joanna.foss@gmail.com>
2022-03-12 18:44:27 -0600

    comments

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Classes;
 
+//implement functions
 Functions fn = new Functions();
 
+//create debtor object from xml file
 Debtor myDebtor = fn.CreateDebtor("test.xml");
 
+//console writeline all fields of the debtor
 fn.printAllDebtorInfo(myDebtor);
