22ad4d8da67b6dd6c7161b580971d684aa89f0f3
Joanna <m.joanna.foss@gmail.com>
2022-03-12 18:56:09 -0600

    try/catch and nullable fields

--- MyApp/Functions.cs
+++ MyApp/Functions.cs
@@ -4,129 +4,139 @@ namespace Classes
 {
     class Functions
     {
+        public Functions(){}
+
         public Debtor CreateDebtor(String filePath)
         {
             XElement xelement = XElement.Load(filePath);
             Debtor currentDebtor = new Debtor();
 
-            foreach(XElement element in xelement.Descendants("field"))
+            foreach (XElement element in xelement.Descendants("field"))
             {
-                if((string) element.Attribute("Name") == "Debtor")
+                try
                 {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Name = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor SSN")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SSN = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor Address 1")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Address1 = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor Address 2")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Address2 = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor Address 3")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Address3 = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor County")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.County = value;
-                }
-                if((string) element.Attribute("Name") == "DebtorEMail")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Email = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor Date of Birth")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.DOB = value;
-                }
-                if((string) element.Attribute("Name") == "DebtorCellPhone")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.CellPhone = value;
-                }
-                if((string) element.Attribute("Name") == "Spouse")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Spouse = value;
-                }
-                if((string) element.Attribute("Name") == "Spouse SSN")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SpouseSSN = value;
-                }
-                if((string) element.Attribute("Name") == "Spouse Address 1")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SpouseAddress1 = value;
-                }
-                if((string) element.Attribute("Name") == "Spouse Address 2")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SpouseAddress2 = value;
-                }
-                if((string) element.Attribute("Name") == "Debtor County")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.DebtorCounty = value;
-                }
-                if((string) element.Attribute("Name") == "SpouseEMail")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SpouseEmail = value;
-                }
-                if((string) element.Attribute("Name") == "Spouse Date of Birth")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SpouseDOB = value;
-                }
-                if((string) element.Attribute("Name") == "SpouseCellPhone")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.SpouseCellPhone = value;
-                }
-                if((string) element.Attribute("Name") == "Chapter")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.Chapter = value;
-                }
-                if((string) element.Attribute("Name") == "Type of Debtor")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.DebtorType = value;
-                }
-                if((string) element.Attribute("Name") == "Attorney Bar Id")
-                {
-                    string value = (string) element.Attribute("Value");
-                    currentDebtor.AttorneyBarId = value;
-                }
-                if((string) element.Attribute("Name") == "IsActive")
-                {
-                    string value = (string) element.Attribute("Value");
-                    if(value == "true")
+                    if ((string)element.Attribute("Name") == "Debtor")
                     {
-                        currentDebtor.IsActive = true;
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Name = value;
                     }
-                    else
+                    if ((string)element.Attribute("Name") == "Debtor SSN")
                     {
-                        currentDebtor.IsActive = false;
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SSN = value;
                     }
+                    if ((string)element.Attribute("Name") == "Debtor Address 1")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Address1 = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Debtor Address 2")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Address2 = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Debtor Address 3")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Address3 = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Debtor County")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.County = value;
+                    }
+                    if ((string)element.Attribute("Name") == "DebtorEMail")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Email = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Debtor Date of Birth")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.DOB = value;
+                    }
+                    if ((string)element.Attribute("Name") == "DebtorCellPhone")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.CellPhone = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Spouse")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Spouse = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Spouse SSN")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SpouseSSN = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Spouse Address 1")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SpouseAddress1 = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Spouse Address 2")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SpouseAddress2 = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Debtor County")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.DebtorCounty = value;
+                    }
+                    if ((string)element.Attribute("Name") == "SpouseEMail")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SpouseEmail = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Spouse Date of Birth")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SpouseDOB = value;
+                    }
+                    if ((string)element.Attribute("Name") == "SpouseCellPhone")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.SpouseCellPhone = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Chapter")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.Chapter = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Type of Debtor")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.DebtorType = value;
+                    }
+                    if ((string)element.Attribute("Name") == "Attorney Bar Id")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        currentDebtor.AttorneyBarId = value;
+                    }
+                    if ((string)element.Attribute("Name") == "IsActive")
+                    {
+                        string value = (string)element.Attribute("Value");
+                        if (value == "true")
+                        {
+                            currentDebtor.IsActive = true;
+                        }
+                        else
+                        {
+                            currentDebtor.IsActive = false;
+                        }
+                    }
+                }
+                catch (NullReferenceException e)
+                {
+                    Console.WriteLine(e);
                 }
             }
 
             return currentDebtor;
         }
+
         public void printAllDebtorInfo(Debtor debtor)
         {
             Console.WriteLine(debtor.Name);
